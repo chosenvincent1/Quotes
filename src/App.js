efe8ba530c7a96f122ca6736bfe59dfa4c324223
mrchosen <chosenvincent@gmail.com>
2022-10-15 13:24:47 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const [quote, setQuote] = useState('')
+
+  const fetchData = ()=> {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const { advice } = response.data.slip
+      setQuote(advice)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  return (
+    <div className="App">
+      <h1 className='title'>your daily Quote</h1>
+      <div className='quote-card'>
+        <h1 className='quote'>{quote}</h1>
+        <button onClick={fetchData}>Get Quote</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
